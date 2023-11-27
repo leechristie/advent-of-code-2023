61b8d56d25e5dd7e3c7d2c6e6477592a6be11195
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-11-27 20:22:23 +0000

    Create aoc23py

---
+++ aoc23py/hello.py
@@ -0,0 +1,6 @@
+# Advent of Code 2023
+# Dr Lee A. Christie
+# @0x1ac@techhub.social
+
+def say_hello() -> None:
+    print('Hello, Advent of Code 2023!')
---
+++ aoc23py/main.py
@@ -0,0 +1,14 @@
+# Advent of Code 2023
+# Dr Lee A. Christie
+# @0x1ac@techhub.social
+
+
+from hello import say_hello
+
+
+def main() -> None:
+    say_hello()
+
+
+if __name__ == '__main__':
+    main()
