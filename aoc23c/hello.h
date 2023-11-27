cb0503c42e2f12e90c7df5fbc10ece67e71b24f3
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-11-27 20:55:39 +0000

    Create aoc23c

---
+++ aoc23c/hello.h
@@ -0,0 +1,10 @@
+// Advent of Code 2023
+// Dr Lee A. Christie
+// @0x1ac@techhub.social
+
+#ifndef AOC23C_HELLO_H
+#define AOC23C_HELLO_H
+
+void say_hello(void);
+
+#endif //AOC23C_HELLO_H
---
+++ aoc23c/main.c
@@ -0,0 +1,13 @@
+// Advent of Code 2023
+// Dr Lee A. Christie
+// @0x1ac@techhub.social
+
+#include "hello.h"
+
+int main(void) {
+
+    say_hello();
+
+    return 0;
+
+}
