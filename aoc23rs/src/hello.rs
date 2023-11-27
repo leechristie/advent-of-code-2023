af12e1d3bdddca54aa99e44d9b63d9d71fede704
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-11-27 21:45:23 +0000

    Create aoc23rs

---
+++ aoc23rs/src/hello.rs
@@ -0,0 +1,9 @@
+// Advent of Code 2023
+// Dr Lee A. Christie
+// @0x1ac@techhub.social
+
+pub fn say_hello() {
+
+    println!("Hello, Advent of Code 2023!");
+
+}
---
+++ aoc23rs/src/main.rs
@@ -0,0 +1,11 @@
+// Advent of Code 2023
+// Dr Lee A. Christie
+// @0x1ac@techhub.social
+
+mod hello;
+
+fn main() {
+
+    hello::say_hello();
+
+}
