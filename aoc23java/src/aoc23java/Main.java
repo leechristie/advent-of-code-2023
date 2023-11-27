4624ecab53a6856ab70ba2e4477f41ad62d46de3
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-11-27 20:30:09 +0000

    Create aoc23java

---
+++ aoc23java/src/aoc23java/Main.java
@@ -0,0 +1,17 @@
+// Advent of Code 2023
+// Dr Lee A. Christie
+// @0x1ac@techhub.social
+
+package aoc23java;
+
+public final class Main {
+
+    private Main() {
+        throw new AssertionError();
+    }
+
+    public static void main(String[] args) {
+        Hello.sayHello();
+    }
+
+}
