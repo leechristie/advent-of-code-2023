f8c859710ab51e178ef719fdebad1dba43489cb7
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2023-12-24 12:17:53 +0000

    Create day06.py

---
+++ aoc23py/day06.py
@@ -0,0 +1,10 @@
+# Advent of Code 2023
+# Dr Lee A. Christie
+# @0x1ac@techhub.social
+
+
+def solve() -> None:
+    print('Advent of Code 2023')
+    print('Day 1')
+    print(f'Part 1: TODO')
+    print(f'Part 2: TODO')
